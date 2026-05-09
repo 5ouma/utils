489af79c5d9396b9aced5d8d9bbbea0acca15494
Souma <git@5ouma.me>
2026-05-09 16:29:47 +0900

    feat(pre-commit)!: Use Deno as a `upload-git-commit-notion` language
    Manage the hook as type-safe.

---
+++ pre-commit/hooks/upload-git-commit-notion/main.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S deno run -P
+
+import { git } from "@roka/git";
+import gitUrlParse from "git-url-parse";
+import { item } from "@1password/op-js";
+import { Client } from "@notionhq/client";
+
+const range = {
+  from: Deno.env.get("PRE_COMMIT_FROM_REF"),
+  to: Deno.env.get("PRE_COMMIT_TO_REF"),
+};
+const url = (() => {
+  const remoteUrl = Deno.env.get("PRE_COMMIT_REMOTE_URL");
+  if (!remoteUrl) throw new Error("PRE_COMMIT_REMOTE_URL is not set");
+  const { source, full_name } = gitUrlParse(remoteUrl);
+  return `https://${source}/${full_name}`;
+})();
+const commits = await git().commit.log(range);
+
+const [database_id, auth] = (() => {
+  const data = item.get("Notion Git Integration", {
+    fields: { label: ["database-id", "credential"] },
+  });
+  if (!Array.isArray(data)) throw new Error("Data is not an array");
+  return data.map((field) => field.value);
+})();
+const client = new Client({ auth });
+
+for (const commit of commits) {
+  const res = await client.pages.create({
+    parent: { database_id },
+    properties: {
+      Message: { title: [{ type: "text", text: { content: commit.subject } }] },
+      ...(commit.body && {
+        Description: {
+          rich_text: [{ type: "text", text: { content: commit.body } }],
+        },
+      }),
+      Hash: { rich_text: [{ type: "text", text: { content: commit.short } }] },
+      URL: { url },
+      Date: { date: { start: commit.author.date.toInstant().toString() } },
+    },
+  });
+  console.log(res.id);
+}
